5910e13a43854976f06c7fd9ff1e3b4e8a5501d1
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-05-15 22:11:26 +0900

    Hierarchy of the module and class

--- lib/middleman-somemoji/extension.rb
+++ lib/middleman-somemoji/extension.rb
@@ -1,31 +1,33 @@
-# Require core library
 require 'middleman-core'
 
-# Extension namespace
-class MyExtension < ::Middleman::Extension
-  option :my_option, 'default', 'An example option'
+module Middleman
+  module Somemoji
+    class Extension < ::Middleman::Extension
+      option :my_option, 'default', 'An example option'
 
-  def initialize(app, options_hash={}, &block)
-    # Call super to build options from the options_hash
-    super
+      def initialize(app, options_hash={}, &block)
+        # Call super to build options from the options_hash
+        super
 
-    # Require libraries only when activated
-    # require 'necessary/library'
+        # Require libraries only when activated
+        # require 'necessary/library'
 
-    # set up your extension
-    # puts options.my_option
+        # set up your extension
+        # puts options.my_option
+      end
+
+      def after_configuration
+        # Do something
+      end
+
+      # A Sitemap Manipulator
+      # def manipulate_resource_list(resources)
+      # end
+
+      # helpers do
+      #   def a_helper
+      #   end
+      # end
+    end
   end
-
-  def after_configuration
-    # Do something
-  end
-
-  # A Sitemap Manipulator
-  # def manipulate_resource_list(resources)
-  # end
-
-  # helpers do
-  #   def a_helper
-  #   end
-  # end
 end
