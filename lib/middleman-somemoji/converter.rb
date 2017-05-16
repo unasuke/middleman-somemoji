a77b41ec3741b98e90294ffdadfca178678f9a1d
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-05-17 00:35:36 +0900

    Convert emoji syntax to img tag

---
+++ lib/middleman-somemoji/converter.rb
@@ -0,0 +1,21 @@
+require 'somemoji'
+
+module Middleman
+  module Somemoji
+    class Converter
+      def initialize(body, options)
+        @body         = body
+        @provider     = options[:provider]
+        @format       = options[:format]
+        @emoji_class  = options[:emoji_class]
+        @emojis_dir   = options[:emojis_dir]
+      end
+
+      def convert
+        eval("::Somemoji::#{@provider}_emoji_collection").replace_code(@body) do |emoji|
+          %(<img alt="#{emoji.character}" class="#{@emoji_class}" src="#{@emojis_dir}/#{emoji.base_path}.#{@format}")
+        end
+      end
+    end
+  end
+end
--- lib/middleman-somemoji/extension.rb
+++ lib/middleman-somemoji/extension.rb
@@ -1,33 +1,25 @@
 require 'middleman-core'
+require 'middleman-somemoji/converter'
 
 module Middleman
   module Somemoji
     class Extension < ::Middleman::Extension
-      option :my_option, 'default', 'An example option'
+      option :provider,     'apple',         %[Emoji provider (default is 'apple')]
+      option :format,       'png',           %[Emoji format (default is 'png')]
+      option :emoji_class,  'emoji',         %[<img> tag class (default is 'emoji')]
+      option :emojis_dir,   '/images/emoji', %[Emoji images directory path (default is '/images/emoji')]
 
       def initialize(app, options_hash={}, &block)
-        # Call super to build options from the options_hash
         super
 
-        # Require libraries only when activated
-        # require 'necessary/library'
+        require 'somemoji'
 
-        # set up your extension
-        # puts options.my_option
+        @options = options
+
+        app.before_render do |body, path, locs, template_class|
+          ::Middleman::Somemoji::Converter.new(body, @options).convert
+        end
       end
-
-      def after_configuration
-        # Do something
-      end
-
-      # A Sitemap Manipulator
-      # def manipulate_resource_list(resources)
-      # end
-
-      # helpers do
-      #   def a_helper
-      #   end
-      # end
     end
   end
 end
